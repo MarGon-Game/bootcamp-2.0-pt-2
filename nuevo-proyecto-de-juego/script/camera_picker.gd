ae4250b7c37500bd2c2a3d1f6f288975f851b529
chonto2000new <agustin.monarrez.h@gmail.com>
2026-04-29 01:11:12 -0600

    cambios de clase poner tiles

---
+++ nuevo-proyecto-de-juego/script/camera_picker.gd
@@ -0,0 +1,31 @@
+extends Camera3D
+class_name  CAMERA_PICKER
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+#	la variable mouse obtiene la vista de la pantalla y con eso optiene la posicion del mouse
+#	es siempre 2d por que el 3d no existe son los papas no se poner comas xd 
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+#	la posicion del rayo tendra la posicion del mouse, basicamente va apuntar a donde este el mouse
+	
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100
+#	fuerza para actualizar la posicion 
+	ray_cast_3d.force_raycast_update()
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_point = ray_cast_3d.get_collision_point()
+			var cell = collider.local_to_map(collision_point)
+			if Input.is_action_just_pressed("cambio_celda"):
+				collider.set_cell_item(cell,1)
+
+		
+	
--- nuevo-proyecto-de-juego/script/grid_map.gd
+++ nuevo-proyecto-de-juego/script/grid_map.gd
@@ -9,7 +9,18 @@ enum elocetas {Base, Enemigos, Entrada, Items, Limite, Relleno, Salida, Otro}
 
 @onready var grid_map: GridMap = $GridMap
 
-
+var txt_map: String = "
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXSXXXXSXX
+XXSXXXXSXX
+XXXXXXXXXX
+XXSXXXXSXX
+XXXSSSSXXX
+XXXXXXXXXX
+XXXXXXXXXX
+"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -39,3 +50,42 @@ func _pinta_cuadro():
 
 func _cambia_loceta(nX: int,nZ: int, _tipoloceta: elocetas = elocetas.Relleno):
 	grid_map.set_cell_item(Vector3i(nX,0,nZ), _tipoloceta,0)
+
+
+func _generate_world():
+	# 1. Limpiamos cualquier cuadro anterior
+	_limpiarGrid()
+	
+	# 2. Separamos el texto grande en un arreglo, donde cada elemento es una línea
+	var lineas = txt_map.strip_edges().split("\n")
+	
+	print(lineas)
+	
+	# 3. Recorremos las filas (Eje Z)
+	for z in range(lineas.size()):
+		var fila = lineas[z].strip_edges() # Quitamos espacios en blanco por si acaso
+		
+		# 4. Recorremos cada letra de la fila (Eje X)
+		for x in range(fila.length()):
+			var caracter = fila[x]
+			
+			# 5. Mapeamos tu rango original (-5 a 4) 
+			# Como x y z van de 0 a 9, le restamos 5 para centrarlo en el origen (0,0,0)
+			var nX = x - 5
+			var nZ = z - 5
+			
+			# 6. Decidimos qué loceta pintar según el caracter usando 'match' (el switch de Godot)
+			var loceta_a_pintar = elocetas.Relleno # Valor por defecto
+			
+			match caracter:
+				"X":
+					loceta_a_pintar = elocetas.Relleno # o elocetas.Base, según tu diseño
+				"1":
+					loceta_a_pintar = elocetas.Entrada
+				"S":
+					loceta_a_pintar = elocetas.Salida
+				_:
+					loceta_a_pintar = elocetas.Otro # Por si hay un error en el texto
+					
+			# 7. Pintamos en el GridMap
+			grid_map.set_cell_item(Vector3i(nX, 0, nZ), loceta_a_pintar, 0)
